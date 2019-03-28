63e5b523f17452ea5cb86eebd364a0c47e649f9c
Jared Baker <jbaker.1563@gmail.com>
2019-03-27 21:20:47 -0400

    remove any previous go installations

--- install_scripts/install_go.sh
+++ install_scripts/install_go.sh
@@ -8,6 +8,7 @@ echo "Downloading Go version $VERSION"
 wget "https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz"
 
 echo "Installing Go version $VERSION"
+sudo rm -fr /usr/local/go # cleanup any previous installations
 sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
 
 echo "Cleaning up..."
