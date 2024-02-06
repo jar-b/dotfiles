a45a97c15d16f1dc9c177c5aec1a4b9acbcd6e66
Jared Baker <jared.baker@hashicorp.com>
2024-02-06 15:02:01 -0500

    nvim: split config

---
+++ .config/nvim/lua/jar-b/keymaps.lua
@@ -0,0 +1,19 @@
+-- Keymaps
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set("i", "jk", "<Esc>")
+
+vim.keymap.set({ "n", "v" }, ";", ":")
+
+vim.keymap.set("n", "<CR>", "G")
+-- Re-center on PgUp/Down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Re-center on search navigation
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
---
+++ .config/nvim/lua/jar-b/options.lua
@@ -0,0 +1,30 @@
+-- Options
+vim.opt.termguicolors = true
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+vim.opt.list = true
+vim.opt.listchars:append {
+    eol = "↲",
+    tab = '» ',
+    trail = "·",
+}
