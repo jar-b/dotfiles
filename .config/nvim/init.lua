8e852dd7227f30af28f258010a7fa11d7f57668a
Jared Baker <jared.baker@hashicorp.com>
2024-07-17 08:49:35 -0400

    nvim: update lazy install step

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,22 +2,17 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Install `lazy.nvim` plugin manager
---   https://github.com/folke/lazy.nvim
---   `:help lazy.nvim.txt` for more info
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
-end
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
-
 
 -- Configure plugins
 require('lazy').setup({
