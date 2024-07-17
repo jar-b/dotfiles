b6a20f97303f1c7826fb6c0d3bd50c2c273231b3
Jared Baker <jared.baker@hashicorp.com>
2024-07-17 09:29:41 -0400

    nvim: update telescope setup

--- .config/nvim/lua/jar-b/telescope.lua
+++ .config/nvim/lua/jar-b/telescope.lua
@@ -8,6 +8,7 @@ require('telescope').setup {
       }
     }
   },
+
   pickers = {
     buffers = {
       show_all_buffers = true,
@@ -22,19 +23,19 @@ require('telescope').setup {
       }
     }
   },
+
+  extensions = {
+    ['ui-select'] = {
+      require('telescope.themes').get_dropdown(),
+    },
+  },
 }
 
--- Enable telescope fzf native, if installed
+-- Enable telescope extensions if installed
 pcall(require('telescope').load_extension, 'fzf')
+pcall(require('telescope').load_extension, 'ui-select')
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer' })
 vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
@@ -45,5 +46,13 @@ vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' 
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer' })
 
 -- vim: ts=2 sts=2 sw=2 et
