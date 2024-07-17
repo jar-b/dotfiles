2e79663d02a38db8e021d418031f810e6370d7d7
Jared Baker <jared.baker@hashicorp.com>
2024-07-17 09:23:11 -0400

    nvim: update which-key registration syntax

--- .config/nvim/lua/jar-b/lsp.lua
+++ .config/nvim/lua/jar-b/lsp.lua
@@ -41,22 +41,17 @@ local on_attach = function(client, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+require('which-key').add {
+  { "<leader>c",  group = "[C]ode" },
+  { "<leader>d",  group = "[D]ocument" },
+  { "<leader>g",  group = "[G]it" },
+  { "<leader>h",  group = "Git [H]unk",      mode = { 'n', 'v' } },
+  { "<leader>r",  group = "[R]ename" },
+  { "<leader>s",  group = "[S]earch" },
+  { "<leader>t",  group = "[T]oggle" },
+  { "<leader>w",  group = "[W]orkspace" },
+  { "<leader>",   group = "VISUAL <leader>", mode = "v" },
 }
--- register which-key VISUAL mode
--- required for visual <leader>hs (hunk stage) to work
-require('which-key').register({
-  ['<leader>'] = { name = 'VISUAL <leader>' },
-  ['<leader>h'] = { 'Git [H]unk' },
-}, { mode = 'v' })
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
