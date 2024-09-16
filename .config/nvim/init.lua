c513d155736c7466def41a84c32a4699c6eb7959
Jared Baker <jared.baker@hashicorp.com>
2024-09-16 10:29:30 -0400

    nvim: git blame nmap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -127,13 +127,10 @@ require('lazy').setup({
         map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
         map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
         map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
-        map('n', '<leader>hb', function()
-          gs.blame_line { full = false }
-        end, { desc = 'git blame line' })
+        map('n', '<leader>hb', function() gs.blame_line { full = false } end, { desc = 'git blame line' })
+        map('n', '<leader>hB', gs.blame, { desc = 'git blame buffer' })
         map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
-        map('n', '<leader>hD', function()
-          gs.diffthis '~'
-        end, { desc = 'git diff against last commit' })
+        map('n', '<leader>hD', function() gs.diffthis '~' end, { desc = 'git diff against last commit' })
 
         -- Toggles
         map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
