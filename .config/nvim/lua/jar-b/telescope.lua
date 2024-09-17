0cc950c145b6e3ab23b5e81cfaf5f86da35c3726
Jared Baker <jared.baker@hashicorp.com>
2024-09-17 14:03:06 -0400

    nvim: map ctrp-p to telescope find_files

--- .config/nvim/lua/jar-b/telescope.lua
+++ .config/nvim/lua/jar-b/telescope.lua
@@ -45,7 +45,7 @@ vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]re
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
