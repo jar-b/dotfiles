67a994b6405e2a00d662be73a66654249c6b46cb
Jared Baker <jared.baker@hashicorp.com>
2025-07-14 10:31:22 -0400

    nvim: add conform, autoformat init

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,239 +1,280 @@
 -- Set leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 -- Configure plugins
-require('lazy').setup({
-  {
-    'Mofiqul/dracula.nvim',
-    name = 'dracula',
-    config = function()
-      vim.cmd.colorscheme 'dracula'
-    end,
-  },
+require("lazy").setup({
+	{
+		"Mofiqul/dracula.nvim",
+		name = "dracula",
+		config = function()
+			vim.cmd.colorscheme("dracula")
+		end,
+	},
 
-  -- Plugins with no configuration
-  'christoomey/vim-tmux-navigator',
-  'tpope/vim-fugitive',
-  'tpope/vim-sleuth',
-  'tpope/vim-vinegar',
-  'farmergreg/vim-lastplace',
+	-- Plugins with no configuration
+	"christoomey/vim-tmux-navigator",
+	"tpope/vim-fugitive",
+	"tpope/vim-sleuth",
+	"tpope/vim-vinegar",
+	"farmergreg/vim-lastplace",
 
-  -- LSP Configuration & Plugins
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for Neovim
-      -- Mason must be loaded before its dependents so we need to set it up here.
-      { 'mason-org/mason.nvim', opts = {} },
-      'mason-org/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
+	-- LSP Configuration & Plugins
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- Automatically install LSPs and related tools to stdpath for Neovim
+			-- Mason must be loaded before its dependents so we need to set it up here.
+			{ "mason-org/mason.nvim", opts = {} },
+			"mason-org/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
 
-      -- Useful status updates for LSP
-      { 'j-hui/fidget.nvim',    opts = {} },
+			-- Useful status updates for LSP
+			{ "j-hui/fidget.nvim", opts = {} },
 
-      -- Allows extra capabilities provided by blink.cmp
-      'saghen/blink.cmp',
+			-- Allows extra capabilities provided by blink.cmp
+			"saghen/blink.cmp",
 
-      -- Format on save
-      'lukas-reineke/lsp-format.nvim',
-    },
-  },
+			-- Format on save
+			"lukas-reineke/lsp-format.nvim",
+		},
+	},
 
-  -- Autocompletion
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
+	-- Autoformat
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true, lsp_format = "fallback" })
+				end,
+				mode = "",
+				desc = "[F]ormat buffer",
+			},
+		},
+		opts = {
+			notify_on_error = false,
+			format_on_save = function(bufnr)
+				-- Disable "format_on_save lsp_fallback" for languages that don't
+				-- have a well standardized coding style. You can add additional
+				-- languages here or re-enable it for the disabled ones.
+				local disable_filetypes = { c = true, cpp = true }
+				if disable_filetypes[vim.bo[bufnr].filetype] then
+					return nil
+				else
+					return {
+						timeout_ms = 500,
+						lsp_format = "fallback",
+					}
+				end
+			end,
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+		},
+	},
 
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-buffer',
+	-- Autocompletion
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			-- Snippet Engine & its associated nvim-cmp source
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
 
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
-    },
-  },
+			-- Adds LSP completion capabilities
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-buffer",
 
-  -- Gitsigns
-  {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
+			-- Adds a number of user-friendly snippets
+			"rafamadriz/friendly-snippets",
+		},
+	},
 
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
+	-- Gitsigns
+	{
+		-- Adds git related signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			-- See `:help gitsigns.txt`
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+			on_attach = function(bufnr)
+				local gs = package.loaded.gitsigns
 
-        -- Navigation
-        map({ 'n', 'v' }, ']c', function()
-          if vim.wo.diff then
-            return ']c'
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return '<Ignore>'
-        end, { expr = true, desc = 'Jump to next hunk' })
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
 
-        map({ 'n', 'v' }, '[c', function()
-          if vim.wo.diff then
-            return '[c'
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return '<Ignore>'
-        end, { expr = true, desc = 'Jump to previous hunk' })
+				-- Navigation
+				map({ "n", "v" }, "]c", function()
+					if vim.wo.diff then
+						return "]c"
+					end
+					vim.schedule(function()
+						gs.next_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true, desc = "Jump to next hunk" })
 
-        -- Actions
-        -- visual mode
-        map('v', '<leader>hs', function()
-          gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'stage git hunk' })
-        map('v', '<leader>hr', function()
-          gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'reset git hunk' })
-        -- normal mode
-        map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
-        map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
-        map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
-        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
-        map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
-        map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
-        map('n', '<leader>hb', function() gs.blame_line { full = false } end, { desc = 'git blame line' })
-        map('n', '<leader>hB', gs.blame, { desc = 'git blame buffer' })
-        map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
-        map('n', '<leader>hD', function() gs.diffthis '~' end, { desc = 'git diff against last commit' })
+				map({ "n", "v" }, "[c", function()
+					if vim.wo.diff then
+						return "[c"
+					end
+					vim.schedule(function()
+						gs.prev_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true, desc = "Jump to previous hunk" })
 
-        -- Toggles
-        map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-        map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
+				-- Actions
+				-- visual mode
+				map("v", "<leader>hs", function()
+					gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { desc = "stage git hunk" })
+				map("v", "<leader>hr", function()
+					gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { desc = "reset git hunk" })
+				-- normal mode
+				map("n", "<leader>hs", gs.stage_hunk, { desc = "git stage hunk" })
+				map("n", "<leader>hr", gs.reset_hunk, { desc = "git reset hunk" })
+				map("n", "<leader>hS", gs.stage_buffer, { desc = "git Stage buffer" })
+				map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "undo stage hunk" })
+				map("n", "<leader>hR", gs.reset_buffer, { desc = "git Reset buffer" })
+				map("n", "<leader>hp", gs.preview_hunk, { desc = "preview git hunk" })
+				map("n", "<leader>hb", function()
+					gs.blame_line({ full = false })
+				end, { desc = "git blame line" })
+				map("n", "<leader>hB", gs.blame, { desc = "git blame buffer" })
+				map("n", "<leader>hd", gs.diffthis, { desc = "git diff against index" })
+				map("n", "<leader>hD", function()
+					gs.diffthis("~")
+				end, { desc = "git diff against last commit" })
 
-        -- Text object
-        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
-      end,
-    },
-  },
+				-- Toggles
+				map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "toggle git blame line" })
+				map("n", "<leader>td", gs.toggle_deleted, { desc = "toggle git show deleted" })
 
-  -- Statusline
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = 'dracula',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
+				-- Text object
+				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "select git hunk" })
+			end,
+		},
+	},
 
-  -- Bufferline
-  { 'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons', opts = {} },
+	-- Statusline
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		-- See `:help lualine.txt`
+		opts = {
+			options = {
+				icons_enabled = true,
+				theme = "dracula",
+				component_separators = "|",
+				section_separators = "",
+			},
+		},
+	},
 
-  -- Comment visual regions/lines
-  { 'numToStr/Comment.nvim',   opts = {} },
+	-- Bufferline
+	{ "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons", opts = {} },
 
-  -- Display pending keybinds
-  {
-    'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    opts = {
-      -- delay between pressing a key and opening which-key (milliseconds)
-      -- this setting is independent of vim.o.timeoutlen
-      delay = 0,
-      icons = {
-        -- set icon mappings to true if you have a Nerd Font
-        mappings = vim.g.have_nerd_font,
-      },
+	-- Comment visual regions/lines
+	{ "numToStr/Comment.nvim", opts = {} },
 
-      -- Document existing key chains
-      spec = {
-        { "<leader>c", group = "[C]ode" },
-        { "<leader>d", group = "[D]ocument" },
-        { "<leader>g", group = "[G]it" },
-        { "<leader>h", group = "Git [H]unk",      mode = { 'n', 'v' } },
-        { "<leader>r", group = "[R]ename" },
-        { "<leader>s", group = "[S]earch" },
-        { "<leader>t", group = "[T]oggle" },
-        { "<leader>w", group = "[W]orkspace" },
-        { "<leader>",  group = "VISUAL <leader>", mode = "v" },
-      },
-    },
-  },
+	-- Display pending keybinds
+	{
+		"folke/which-key.nvim",
+		event = "VimEnter", -- Sets the loading event to 'VimEnter'
+		opts = {
+			-- delay between pressing a key and opening which-key (milliseconds)
+			-- this setting is independent of vim.o.timeoutlen
+			delay = 0,
+			icons = {
+				-- set icon mappings to true if you have a Nerd Font
+				mappings = vim.g.have_nerd_font,
+			},
 
-  -- ZenMode, Twilight
-  { "folke/zen-mode.nvim",     opts = {} },
-  { "folke/twilight.nvim",     opts = {} },
+			-- Document existing key chains
+			spec = {
+				{ "<leader>c", group = "[C]ode" },
+				{ "<leader>d", group = "[D]ocument" },
+				{ "<leader>g", group = "[G]it" },
+				{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+				{ "<leader>r", group = "[R]ename" },
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>t", group = "[T]oggle" },
+				{ "<leader>w", group = "[W]orkspace" },
+				{ "<leader>", group = "VISUAL <leader>", mode = "v" },
+			},
+		},
+	},
 
-  -- gotests
-  {
-    "yanskun/gotests.nvim",
-    ft = "go",
-    config = function()
-      require("gotests").setup()
-    end,
-  },
+	-- ZenMode, Twilight
+	{ "folke/zen-mode.nvim", opts = {} },
+	{ "folke/twilight.nvim", opts = {} },
 
-  -- Fuzzy Finder (files, lsp, etc)
-  {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-      { 'nvim-telescope/telescope-ui-select.nvim' },
+	-- gotests
+	{
+		"yanskun/gotests.nvim",
+		ft = "go",
+		config = function()
+			require("gotests").setup()
+		end,
+	},
 
-      -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-    },
-  },
+	-- Fuzzy Finder (files, lsp, etc)
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
+			{ "nvim-telescope/telescope-ui-select.nvim" },
 
-  -- Highlight, edit, and navigate code
-  {
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-      'nvim-treesitter/nvim-treesitter-context',
-    },
-    build = ':TSUpdate',
-  },
+			-- Useful for getting pretty icons, but requires a Nerd Font.
+			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+		},
+	},
+
+	-- Highlight, edit, and navigate code
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"nvim-treesitter/nvim-treesitter-context",
+		},
+		build = ":TSUpdate",
+	},
 }, {})
 
 -- Load custom snippets
@@ -247,15 +288,15 @@ require("jar-b.options")
 require("jar-b.keymaps")
 
 -- Telescope
-require('jar-b.telescope')
+require("jar-b.telescope")
 
 -- Treesitter
-require('jar-b.treesitter')
+require("jar-b.treesitter")
 
 -- LSP
-require('jar-b.lsp')
+require("jar-b.lsp")
 
 -- Autocompletion
-require('jar-b.cmp')
+require("jar-b.cmp")
 
 -- vim: ts=2 sts=2 sw=2 et
