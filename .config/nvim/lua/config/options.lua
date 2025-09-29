5233ca010336045fbad0b9e8c01bbd4885ad0978
Jared Baker <jared.baker@hashicorp.com>
2025-09-29 15:06:24 -0400

    nvim: disable some default plugins, add vim-vinegar back

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.list = true
+vim.opt.listchars:append({
+  eol = "↲",
+  tab = "» ",
+  trail = "·",
+})
+
+vim.filetype.add({
+  extension = {
+    gtpl = "gotmpl",
+    hcl = "terraform",
+  },
+})
--- .config/nvim/lua/plugins/custom.lua
+++ .config/nvim/lua/plugins/custom.lua
@@ -11,26 +11,40 @@ return {
         gopls = {
           settings = {
             gopls = {
-              -- Set back to gopls default
+              -- Set back to gopls defaults
               -- https://tip.golang.org/gopls/settings#directoryfilters-string
               directoryFilters = { "-**/node_modules" },
+              -- https://tip.golang.org/gopls/settings#analyses-mapstringbool
+              -- Need to explicitly set these to false. Providing an empty list just
+              -- merges nothing with the explicitly configured analyses from the lang.go
+              -- extra.
+              analyses = {
+                nilness = false,
+                unusedparams = false,
+                unusedwrite = false,
+                useany = false,
+              },
               -- semanticTokens cause gopls to hog memory in large projects (terraform-provider-aws)
               semanticTokens = false,
+
+              staticcheck = false,
             },
           },
         },
       },
     },
   },
+
   -- Formatting
   {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        go = { "goimports" },
+        go = { "goimports" }, -- gofumpt configured by default, remove it
       },
     },
   },
+
   -- Plugin Keymaps
   {
     "nvim-telescope/telescope.nvim",
@@ -38,6 +52,7 @@ return {
       { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
     },
   },
+
   -- Colorscheme
   { "Mofiqul/dracula.nvim" },
   {
@@ -46,14 +61,18 @@ return {
       colorscheme = "dracula",
     },
   },
-  -- UI
+
+  -- Additional non-extras
+  { "tpope/vim-vinegar" },
+
+  -- Disabled default plugins
   {
     "folke/snacks.nvim",
     opts = {
-      -- disable word highlighting
-      words = {
-        enabled = false,
-      },
+      explorer = { enabled = false }, -- prefer vim-vinegar instead
+      indent = { enabled = false },
+      words = { enabled = false },
     },
   },
+  -- { "folke/noice.nvim", enabled = false },
 }
