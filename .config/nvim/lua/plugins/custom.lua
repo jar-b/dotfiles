563c58d0f7c7feb05a863c485f730f279b2a875a
Jared Baker <jared.baker@hashicorp.com>
2026-05-15 16:04:04 -0400

    nvim: disable golangci-lint linter default

--- .config/nvim/lua/plugins/custom.lua
+++ .config/nvim/lua/plugins/custom.lua
@@ -1,86 +1,96 @@
 -- Customization to be merged with default plugin behavior
 return {
-  -- LSP
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      inlay_hints = {
-        enabled = false,
-      },
-      servers = {
-        gopls = {
-          settings = {
-            gopls = {
-              -- Set back to gopls defaults
-              -- https://tip.golang.org/gopls/settings#directoryfilters-string
-              directoryFilters = { "-**/node_modules" },
-              -- https://tip.golang.org/gopls/settings#analyses-mapstringbool
-              -- Need to explicitly set these to false. Providing an empty list just
-              -- merges nothing with the explicitly configured analyses from the lang.go
-              -- extra.
-              analyses = {
-                nilness = false,
-                unusedparams = false,
-                unusedwrite = false,
-                useany = false,
-              },
-              -- semanticTokens cause gopls to hog memory in large projects (terraform-provider-aws)
-              semanticTokens = false,
+	-- LSP
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			inlay_hints = {
+				enabled = false,
+			},
+			servers = {
+				gopls = {
+					settings = {
+						gopls = {
+							-- Set back to gopls defaults
+							-- https://tip.golang.org/gopls/settings#directoryfilters-string
+							directoryFilters = { "-**/node_modules" },
+							-- https://tip.golang.org/gopls/settings#analyses-mapstringbool
+							-- Need to explicitly set these to false. Providing an empty list just
+							-- merges nothing with the explicitly configured analyses from the lang.go
+							-- extra.
+							analyses = {
+								nilness = false,
+								unusedparams = false,
+								unusedwrite = false,
+								useany = false,
+							},
+							-- semanticTokens cause gopls to hog memory in large projects (terraform-provider-aws)
+							semanticTokens = false,
 
-              staticcheck = false,
-            },
-          },
-        },
-      },
-    },
-  },
+							staticcheck = false,
+						},
+					},
+				},
+			},
+		},
+	},
 
-  -- Formatting
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        go = { "goimports" }, -- gofumpt configured by default, remove it
-      },
-    },
-  },
+	-- Formatting
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				go = { "goimports" }, -- gofumpt configured by default, remove it
+			},
+		},
+	},
 
-  -- Snippets
-  {
-    "L3MON4D3/LuaSnip",
-    config = function()
-      require("luasnip.loaders.from_lua").load({ paths = { "./snippets" } })
-    end,
-  },
+	-- Linting
+	{
+		"mfussenegger/nvim-lint",
+		opts = {
+			linters_by_ft = {
+				go = {}, -- golangci-lint on by default, remove it
+			},
+		},
+	},
 
-  -- Plugin Keymaps
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-    },
-  },
+	-- Snippets
+	{
+		"L3MON4D3/LuaSnip",
+		config = function()
+			require("luasnip.loaders.from_lua").load({ paths = { "./snippets" } })
+		end,
+	},
 
-  -- Colorscheme
-  { "Mofiqul/dracula.nvim" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "dracula",
-    },
-  },
+	-- Plugin Keymaps
+	{
+		"nvim-telescope/telescope.nvim",
+		keys = {
+			{ "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+		},
+	},
 
-  -- Additional non-extras
-  { "tpope/vim-vinegar" },
+	-- Colorscheme
+	{ "Mofiqul/dracula.nvim" },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "dracula",
+		},
+	},
 
-  -- Disabled default plugins
-  {
-    "folke/snacks.nvim",
-    opts = {
-      explorer = { enabled = false }, -- prefer vim-vinegar instead
-      indent = { enabled = false },
-      words = { enabled = false },
-    },
-  },
-  { "nvim-mini/mini.pairs", enabled = false },
+	-- Additional non-extras
+	{ "tpope/vim-vinegar" },
+
+	-- Disabled default plugins
+	{
+		"folke/snacks.nvim",
+		opts = {
+			explorer = { enabled = false }, -- prefer vim-vinegar instead
+			indent = { enabled = false },
+			words = { enabled = false },
+		},
+	},
+	{ "nvim-mini/mini.pairs", enabled = false },
 }
